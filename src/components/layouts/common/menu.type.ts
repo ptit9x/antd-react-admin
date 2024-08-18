2eace0b206a427c1ea70a041bf7d3aadedd013d9
Richard Do <ngochuynh1991@gmail.com>
2024-08-18 11:26:31 +0700

    feat: switch language

--- src/components/layouts/common/menu.type.ts
+++ src/components/layouts/common/menu.type.ts
@@ -3,11 +3,7 @@ import { ReactNode } from "react";
 type MenuItem = {
   /** menu item code */
   code: string;
-  /** menu labels */
-  label: {
-    zh_CN: string;
-    en_US: string;
-  };
+  label: string;
   icon?: ReactNode;
   path: string;
   children?: MenuItem[];
---
+++ src/routes/routes.path.ts
@@ -0,0 +1,9 @@
+export const PATH_LOGIN = '/login';
+export const PATH_HOME = '/dashboard';
+export const PATH_DOCUMENTATION = '/documentation';
+export const PATH_GUIDE = '/guide';
+
+// components page
+export const PATH_COMPONENT_FORM = '/component/form';
+export const PATH_COMPONENT_TABLE = '/component/table';
+export const PATH_COMPONENT_TABS = '/component/tabs';
